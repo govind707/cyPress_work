d1d566e5f10c0faa3f010c25dd8600743954bf65
govind707 <govind.singh@krazybee.com>
2020-10-13 18:56:31 +0530

    snapshot testing with cypress

--- krazybeeTesting/cypress/integration/Ecomm_scrapping/flipkartScrapping_spec.js
+++ krazybeeTesting/cypress/integration/Ecomm_scrapping/flipkartScrapping_spec.js
@@ -32,7 +32,7 @@ describe("EcommScrapping-Flipkart Testing", () => {
 
        // cy.wait(2000)
 
-        cy.get('.skins__PdtListing-QkWkM.BcfPC').as('loan_info')
+        cy.get('.skins__PdtListing-QkWkM.BcfPC',{ failOnStatusCode: false }).as('loan_info')
 
         cy.get('@loan_info')
             .children()
@@ -58,29 +58,77 @@ describe("EcommScrapping-Flipkart Testing", () => {
             .should('contain', 'Additional Information')   
             .click() 
 
-        cy.get('#app > div > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div.skins__SummaryCon-eWQpKA.gXiRNK').as('profile_addInfo')
+        cy.get('#app > div > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div.skins__SummaryCon-eWQpKA.gXiRNK > div:nth-child(5) > div').as('profile_addInfo_ecom')
         
 
-        cy.get('@profile_addInfo')
-        .should('have.length',6)
+        cy.get('@profile_addInfo_ecom')
+        .should('contain','E-commerce Connect')
 
-            
-          //  .should('have.css','')
+        cy.get('@profile_addInfo_ecom')
+        .children()
+        .should('have.length',2)
 
-        //   cy.get('@profile_summary')
-        //   .children()
-        //   .eq(4)
-        //   .children()   
-        //   .children()  
-        //   .eq(1)
-        //   .children()
-        //   .should('have.css','width','10%')
+        cy.get('@profile_addInfo_ecom')
+        .children()
+        .eq(0)
+        .children()
+        .children()
+        .children()
+        .should('have.length',3)
+        .eq(2)
+        .children()
+        .should('have.css','color','rgb(242, 177, 0)') // assertion for testing arrow
 
-        
-        // .then( wdth => {
-        //   return  wdth
-        // }).should('eq','10%')
-       // .should('have.value','width','10%')
+
+        cy.get('@profile_addInfo_ecom')
+        .click()
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div > div.skins__StyledOptions-flVgwh.jxcLYf.skins__PDSelFixed-ebFJqK.evbfZN').as('amazon-flipkart')
+
+        cy.get('@amazon-flipkart')
+        .children()
+        .should('have.length',3)
+
+        cy.get('@amazon-flipkart')
+        .children()
+        .eq(1)
+        .should('contain','Use Flipkart Account')
+        .click()
+
+        cy.get('@amazon-flipkart')
+        .children()
+        .eq(2)
+        .click()
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div.skins__StyledHeader-bARYhf.JRMTC')
+        .should('contain','Flipkart Sync')
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div.skins__StyledMiddleSection-fQnHBy.ksnYpt > div > div.skins__StyledSteps-kKtHyM.iTkqfP')
+        .should('contain','STEPS')
+        .siblings()
+        .children() //table
+        .children() //tbody
+        .children() // rows containing paragraphs
+        .should('have.length',2) 
+
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > button')
+        .should('have.css','background-color','rgb(253, 213, 53)')
+        .should('have.text',' Continue ')
+        .click()
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.cFgZAX > div > form > div.BInput__InCon-UCTdV.JspVP > table > tbody > tr > td.BInput__ErrCon-bianUW.kURtGg > input')
+        .should('have.attr','placeholder','Flipkart Account Mobile Number')
+        .type(Cypress.env('Mobile_no'))
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.cFgZAX > div > form > div.BInput__InputBox-cDivfo.bTceFM > table > tbody > tr > td.BInput__ErrCon-bianUW.kURtGg > input')
+        .should('have.attr','placeholder','Flipkart  account password')
+        .type(Cypress.env('Pass'),{log:false})
+
+        cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.cFgZAX > div > form > button')
+        .should('have.text','Submit')
+        .should('have.css','background-color','rgb(253, 213, 53)')
+        //.click()
     })    
 
 
@@ -101,28 +149,6 @@ describe("EcommScrapping-Flipkart Testing", () => {
 
         cy.visit('/' + 'login', { failOnStatusCode: false })
 
-        // cy.wait(2000)
-
-
-
-        // cy.get('#app > div > div > div > div > div:nth-child(3) > div.skins__LoginBtnCon-fIoCIg.bfKfLI > div > div > div:nth-child(3) > div > a > a > span') 
-        // .should('contain','Login with Registered Mobile')
-        // .click()
-
-
-        // cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div > form > div.BInput__InCon-UCTdV.JspVP > table > tbody > tr > td.BInput__ErrCon-bianUW.kURtGg > input')
-        // .should('have.attr','placeholder','Enter your registered mobile number')
-        // .type(7477221726)
-
-        // cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div > form > div.skins__OtpCon-gQOXOO.eVXRnx > button')
-        // .should('contain','Get OTP')
-        // .click()
-
-        // cy.wait(25000)
-
-        // cy.get('#app > div > div > div > div.BPage__BodyCon-hjhObv.giSHwH > div > form > button')
-        // .should('contain','Submit')
-        // .click()
 
         cy.get('.skins__PdtListing-QkWkM.BcfPC').as('loan_info')
 
---
+++ krazybeeTesting/cypress/integration/kreditBeeAppTesting/kb_snapShotTesting_spec.js
@@ -0,0 +1,36 @@
+// import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+// addMatchImageSnapshotCommand();
+// addMatchImageSnapshotPlugin(on, config);
+
+describe("kreditBee App Snapshot Testing ", () => {
+
+    it("snapshots", () => {
+        cy.visit('/' + 'login', { failOnStatusCode: false })
+
+        cy.wait(2000)
+
+        cy.matchImageSnapshot('home')
+
+
+        cy.get('.skins__PdtListing-QkWkM.BcfPC', { failOnStatusCode: false }).as('loan_info')
+
+        cy.get('@loan_info')
+            .children()
+            .eq(3)
+            .should('contain', 'Continue Application')
+            .matchImageSnapshot('before-clicking-continue-button')
+           // .click()
+
+
+        cy.get('@loan_info')
+            .children()
+            .eq(3)
+            .click({force:true})
+        // cy.wait(2000)    
+        // cy.matchImageSnapshot('after-clicking-continue-button')
+
+        //cy.wait(5000)
+
+    })
+})
